83ab8c5e1aaf6063942f9283f3f0c127252fd9b1
thecanabrava <vcanabravatr@gmail.com>
2020-07-11 14:30:59 -0300

    Added spike

---
+++ Assets/Scripts/SpikeBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeBehaviour : MonoBehaviour
+{
+
+    public WorldScript worldScript;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.name.Equals("Player"))
+        {
+            worldScript.load("MainMenu");
+        }
+        else if(collider.name.Equals("Enemy"))
+        {
+            EnemyMovement enemy = collider.transform.gameObject.GetComponent<EnemyMovement>();
+            enemy.destroySelf();
+        }
+
+    }
+}
