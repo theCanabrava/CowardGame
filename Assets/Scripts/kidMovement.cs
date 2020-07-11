a9d6157111112e980ac36c30c88c3cb348315720
thecanabrava <vcanabravatr@gmail.com>
2020-07-11 10:15:30 -0300

    Detected collision direction

--- Assets/Scripts/kidMovement.cs
+++ Assets/Scripts/kidMovement.cs
@@ -8,12 +8,37 @@ public class kidMovement : MonoBehaviour
 
     public Rigidbody2D rB;
 
-    public PolygonCollider2D collider;
-
     Vector2 movement;
 
-    private void OnTriggerEnter2D(Collider2D collider){
-    	Debug.Log("OH MY GOD!");
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+    	reactToCollision(collider);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        reactToCollision(collision.collider);   
+    }
+
+    private void reactToCollision(Collider2D collider)
+    {
+        Debug.Log("OH MY GOD!");
+        Vector2 enemyPosition = collider.transform.gameObject.transform.position;
+
+        float deltaX = rB.position.x - enemyPosition.x;
+        float deltaY = rB.position.y - enemyPosition.y;
+
+        if(Mathf.Abs(deltaX) >= Mathf.Abs(deltaY))
+        {
+            if(deltaX > 0)  Debug.Log("I should go right");
+            else Debug.Log("I should go left");
+        }
+        else
+        {
+            if(deltaY > 0)  Debug.Log("I should go up");
+            else Debug.Log("I should go down");
+        }
     }
 
     void Update()
