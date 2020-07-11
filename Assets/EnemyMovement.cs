296443a692bd0903e8ba3b93ffdb79542f66f26d
thecanabrava <vcanabravatr@gmail.com>
2020-07-10 23:56:06 -0300

    Added EnemyMovement

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidBody;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
