b6f015155eeb20513ac27e0f306c3368f7fe174d
thecanabrava <vcanabravatr@gmail.com>
2020-07-11 11:24:19 -0300

    Added world script

--- Assets/Scripts/DoorBehaviour.cs
+++ Assets/Scripts/DoorBehaviour.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DoorBehaviour : MonoBehaviour
 {
+    public WorldScript worldScript;
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.name.Equals("Player"))
         {
             Debug.Log("Game is done");
+            worldScript.ping();
         }
     }
 }
---
+++ Assets/Scripts/WorldScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldScript : MonoBehaviour
+{
+    public void ping()
+    {
+        Debug.Log("My children are calling me!");
+    }
+}
