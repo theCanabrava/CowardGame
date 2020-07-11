bf2cac7667669fc774691f1defc55a15c8538a86
BoltSheep <thidampe@gmail.com>
2020-07-11 01:40:22 -0300

    add os outro personagem

---
+++ Assets/Scripts/kidMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kidMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rB;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+
+   // void OnCollisionEnter(Collision corpo)
+   // {	
+
+    	//rB.isKinematic = false;
+
+   // }
+
+   // void OnCollisionExit (Collision corpo) 
+    //{
+    	//rB.isKinematic = true;
+   // }
+
+    void Update()
+    {
+      
+
+    }
+    void FixedUpdate()
+    {
+        
+    }
+}
